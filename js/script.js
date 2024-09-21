6a5ee04e527a7f23af4125928904eccf706c2e41
ahmedredakilany <74866215+ahmedredakilany@users.noreply.github.com>
2024-09-22 02:35:36 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 document.getElementById("submit-btn").addEventListener("click", function () {
   window.location.href = "https://t.co/NWDtLTM3K2";
 });
+
+const phoneNumberInput = document.getElementById("phone-number");
+const submitButton = document.getElementById("submit-btn");
+
+// وظيفة لتحديث حالة الزر
+function updateButtonState() {
+  submitButton.disabled = phoneNumberInput.value.trim() === "";
+}
+
+// إضافة مستمع حدث للتحديث عند إدخال أي شيء في حقل الرقم
+phoneNumberInput.addEventListener("input", updateButtonState);
+
+// استدعاء الدالة لتحديث حالة الزر عند تحميل الصفحة
+updateButtonState();
