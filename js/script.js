8c2b32407662c49b5fd40846269be4a44eb0a2cb
ahmedredakilany <74866215+ahmedredakilany@users.noreply.github.com>
2024-09-22 03:39:30 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,17 +1,43 @@
-document.getElementById("submit-btn").addEventListener("click", function () {
-  window.location.href = "https://t.co/NWDtLTM3K2";
-});
-
 const phoneNumberInput = document.getElementById("phone-number");
 const submitButton = document.getElementById("submit-btn");
 
-// وظيفة لتحديث حالة الزر
-function updateButtonState() {
-  submitButton.disabled = phoneNumberInput.value.trim() === "";
-}
+submitButton.addEventListener("click", function (event) {
+  event.preventDefault();
 
-// إضافة مستمع حدث للتحديث عند إدخال أي شيء في حقل الرقم
-phoneNumberInput.addEventListener("input", updateButtonState);
+  if (phoneNumberInput.value.trim() === "") {
+    swal({
+      title: "تنبيه",
+      text: "يرجى كتابة رقم الهاتف.",
+      icon: "warning",
+      button: "حسناً",
+    });
+  } else {
+    // استخدام SweetAlert عند النجاح
+    swal({
+      title: "نجاح",
+      text: "تم تسجيل رقم الهاتف بنجاح!",
+      icon: "success",
+      button: "حسناً",
+    }).then(() => {
+      window.location.href = "https://www.google.com";
+    });
+  }
+});
 
-// استدعاء الدالة لتحديث حالة الزر عند تحميل الصفحة
-updateButtonState();
+document.addEventListener("DOMContentLoaded", () => {
+  // تغيير صورة العلم بناءً على الاختيار
+  document
+    .getElementById("service-provider2")
+    .addEventListener("change", function () {
+      const selectedValue = this.value;
+      const flagImg = document.getElementById("flag");
+
+      if (selectedValue === "om") {
+        flagImg.src = "imgs/عمان.png";
+      } else if (selectedValue === "uae") {
+        flagImg.src = "imgs/امارات.png";
+      } else if (selectedValue === "kuwait") {
+        flagImg.src = "imgs/الكويت.png";
+      }
+    });
+});
